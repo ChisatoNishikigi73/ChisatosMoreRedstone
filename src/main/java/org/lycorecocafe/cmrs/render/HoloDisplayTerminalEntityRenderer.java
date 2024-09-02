fb0131dc46c730c627dd2772c48251413ea85dcc
ChisatoNishikigi73 <89033115+chisatonishikigi73@users.noreply.github.com>
2024-09-03 02:17:58 +0800

    [1.0.6-beta] Add culling faces for area blocks

--- src/main/java/org/lycorecocafe/cmrs/render/HoloDisplayTerminalEntityRenderer.java
+++ src/main/java/org/lycorecocafe/cmrs/render/HoloDisplayTerminalEntityRenderer.java
@@ -1,18 +1,25 @@
 package org.lycorecocafe.cmrs.render;
 
 import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.blaze3d.vertex.VertexConsumer;
 import com.mojang.math.Vector3f;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.Font;
-import net.minecraft.client.renderer.LightTexture;
 import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.block.BlockRenderDispatcher;
+import net.minecraft.client.renderer.block.model.BakedQuad;
 import net.minecraft.client.renderer.blockentity.BlockEntityRenderer;
 import net.minecraft.client.renderer.blockentity.BlockEntityRendererProvider;
 import net.minecraft.client.renderer.entity.EntityRenderDispatcher;
 import net.minecraft.client.renderer.texture.OverlayTexture;
+import net.minecraft.client.resources.model.BakedModel;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
 import net.minecraft.nbt.CompoundTag;
+import net.minecraft.util.RandomSource;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.world.inventory.InventoryMenu;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.AABB;
@@ -20,18 +27,22 @@ import net.minecraft.world.phys.Vec3;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.client.model.data.ModelData;
-import net.minecraftforge.client.model.data.ModelProperty;
+import org.jetbrains.annotations.NotNull;
 import org.lycorecocafe.cmrs.blockentity.holo.HoloDisplayTerminalBlockEntity;
 import org.lycorecocafe.cmrs.utils.game.entity.EntityHelper;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Objects;
+import java.util.Set;
 
+import static net.minecraft.client.renderer.LightTexture.FULL_BRIGHT;
 import static org.lycorecocafe.cmrs.blocks.HoloDisplayTerminalBlock.FACING;
 
 @OnlyIn(Dist.CLIENT)
 public class HoloDisplayTerminalEntityRenderer implements BlockEntityRenderer<HoloDisplayTerminalBlockEntity> {
 
-    public static final ModelProperty<BlockState> DISPLAY_BLOCK_STATE = new ModelProperty<>();
+    public static final RenderType HOLO_DISPLAY = RenderType.entityTranslucent(InventoryMenu.BLOCK_ATLAS);
     private float rotateSpeed = 0f;
 
     public HoloDisplayTerminalEntityRenderer(BlockEntityRendererProvider.Context context) {
@@ -39,29 +50,24 @@ public class HoloDisplayTerminalEntityRenderer implements BlockEntityRenderer<Ho
     }
 
     //TODO: 超过基岩之后基岩之上的方块也不会渲染
-    //TODO: 剔除剔除面
+    //TODO: 追踪生物
     @Override
-    public void render(HoloDisplayTerminalBlockEntity blockEntity, float partialTicks, PoseStack poseStack, MultiBufferSource buffer, int combinedLight, int combinedOverlay) {
+    public void render(HoloDisplayTerminalBlockEntity blockEntity, float partialTicks, @NotNull PoseStack poseStack, @NotNull MultiBufferSource buffer, int combinedLight, int combinedOverlay) {
         // 获取要显示的方块区域状态
         this.rotateSpeed = blockEntity.getRotateSpeed();
-        BlockState[][][] blockStates = blockEntity.getDisplayBlockStates();
         HoloDisplayTerminalBlockEntity.MODE mode = blockEntity.getMode();
-
-        if (mode.equals(HoloDisplayTerminalBlockEntity.MODE.OFFLINE)) {
-            renderTextAboveBlock(blockEntity, partialTicks, "Player Offline...", poseStack, buffer, combinedLight);
-            return;
+        switch (mode) {
+            case AREA, TRACKER -> renderArea(blockEntity, poseStack, buffer, partialTicks, FULL_BRIGHT);
+            case MODEL -> renderModel(blockEntity, poseStack, buffer, partialTicks, FULL_BRIGHT);
+            case OFFLINE ->
+                    renderTextAboveBlock(blockEntity, partialTicks, "Player Offline...", poseStack, buffer, FULL_BRIGHT);
         }
+    }
 
-        if (mode.equals(HoloDisplayTerminalBlockEntity.MODE.MODEL)) {
-            renderModel(blockEntity, poseStack, buffer, partialTicks, combinedLight);
-            return;
-        }
+    private void renderArea(HoloDisplayTerminalBlockEntity blockEntity, PoseStack poseStack, MultiBufferSource buffer, float partialTicks, int combinedLight) {
+        BlockState[][][] blockStates = blockEntity.getDisplayBlockStates();
+        if (blockStates.length == 0) return;
 
-        if (blockStates.length == 0) {
-            return;
-        }
-
-        // 定义放大比例
         double width = blockEntity.getDisplayRange().getXsize();
         double depth = blockEntity.getDisplayRange().getZsize();
 
@@ -74,11 +80,11 @@ public class HoloDisplayTerminalEntityRenderer implements BlockEntityRenderer<Ho
         }
 
         // 使用游戏内时间和 partialTicks 来平滑旋转
-        float rotation = (blockEntity.getLevel().getGameTime() + partialTicks) * rotateSpeed % 360;
+        float rotation = (Objects.requireNonNull(blockEntity.getLevel()).getGameTime() + partialTicks) * rotateSpeed % 360;
 
         // 应用整体旋转和缩放
         poseStack.pushPose();
-        poseStack.translate(0.5, (double) 5 / 16, 0.5); // 提升全息投影的位置
+        poseStack.translate(0.5, (double) 5 / 16, 0.5);
         poseStack.mulPose(Vector3f.YP.rotationDegrees(rotation));
         poseStack.mulPose(Vector3f.YP.rotationDegrees(blockEntity.getBlockState().getValue(FACING).getStepX() * 90));
         poseStack.scale(scale, scale, scale);
@@ -87,16 +93,39 @@ public class HoloDisplayTerminalEntityRenderer implements BlockEntityRenderer<Ho
         double halfX = blockStates.length / 2.0;
         double halfZ = blockStates[0][0].length / 2.0;
 
-        // 渲染区域内的所有方块
         BlockRenderDispatcher blockRenderer = Minecraft.getInstance().getBlockRenderer();
+        VertexConsumer buffer2 = buffer.getBuffer(HOLO_DISPLAY);
+
         for (int x = 0; x < blockStates.length; x++) {
             for (int y = 0; y < blockStates[0].length; y++) {
                 for (int z = 0; z < blockStates[0][0].length; z++) {
                     BlockState blockState = blockStates[x][y][z];
-                    if (blockState != null) {
+                    if (blockState != null && !blockState.isAir()) {
+                        RandomSource random = RandomSource.create(639);
+                        BlockPos relativePos = new BlockPos(x - halfX, y, z - halfZ);
+                        BakedModel model = blockRenderer.getBlockModel(blockState);
+
                         poseStack.pushPose();
-                        poseStack.translate(x - halfX, y, z - halfZ);
-                        blockRenderer.renderSingleBlock(blockState, poseStack, buffer, LightTexture.FULL_BRIGHT, OverlayTexture.NO_OVERLAY);
+                        poseStack.translate(relativePos.getX(), relativePos.getY(), relativePos.getZ());
+
+                        Set<Direction> renderedFaces = new HashSet<>();
+
+                        for (Direction direction : Direction.values()) {
+                            if (shouldRenderFace(blockStates, x, y, z, direction) && !renderedFaces.contains(direction)) {
+                                List<BakedQuad> quads = model.getQuads(blockState, direction, random, ModelData.EMPTY, RenderType.cutout());
+                                for (BakedQuad quad : quads) {
+                                    renderSignalQuad(poseStack, combinedLight, buffer2, blockState, quad);
+                                }
+                                renderedFaces.add(direction);
+                            }
+                        }
+
+                        // 渲染没有特定方向的 quads（通常是方块的主体部分）
+                        List<BakedQuad> generalQuads = model.getQuads(blockState, null, random, ModelData.EMPTY, RenderType.cutout());
+                        for (BakedQuad quad : generalQuads) {
+                            renderSignalQuad(poseStack, combinedLight, buffer2, blockState, quad);
+                        }
+
                         poseStack.popPose();
                     }
                 }
@@ -107,51 +136,66 @@ public class HoloDisplayTerminalEntityRenderer implements BlockEntityRenderer<Ho
         AABB displayRange = blockEntity.getDisplayRange();
         List<Entity> entities = blockEntity.getLevel().getEntities(null, displayRange);
         EntityRenderDispatcher renderer = Minecraft.getInstance().getEntityRenderDispatcher();
-        Vec3 center = displayRange.getCenter(); // 获取显示区域的中心点
+        Vec3 center = displayRange.getCenter();
         for (Entity entity : entities) {
             poseStack.pushPose();
             poseStack.translate(entity.getX() - center.x, entity.getY() - center.y + 3 + displayRange.getYsize() / 2 - 3, entity.getZ() - center.z);
-            renderer.render(entity, 0, 0, 0, 0, partialTicks, poseStack, buffer, LightTexture.FULL_BRIGHT);
+            poseStack.translate(-1, 0, -1);
+            renderer.render(entity, 0, 0, 0, 0, partialTicks, poseStack, buffer, combinedLight);
             poseStack.popPose();
         }
 
-        poseStack.popPose(); // 恢复原始变换
+        poseStack.popPose();
+    }
+
+    private void renderSignalQuad(PoseStack poseStack, int combinedLight, VertexConsumer buffer2, BlockState blockState, BakedQuad quad) {
+        int color = quad.getTintIndex() != -1 ?
+                Minecraft.getInstance().getBlockColors().getColor(blockState, null, null, quad.getTintIndex()) :
+                0xFFFFFF;
+        float r = (color >> 16 & 255) / 255.0F;
+        float g = (color >> 8 & 255) / 255.0F;
+        float b = (color & 255) / 255.0F;
+        buffer2.putBulkData(poseStack.last(), quad, r, g, b, combinedLight, OverlayTexture.NO_OVERLAY);
+    }
+
+    private boolean shouldRenderFace(BlockState[][][] blockStates, int x, int y, int z, Direction direction) {
+        int nx = x + direction.getStepX();
+        int ny = y + direction.getStepY();
+        int nz = z + direction.getStepZ();
+
+        if (nx < 0 || nx >= blockStates.length || ny < 0 || ny >= blockStates[0].length || nz < 0 || nz >= blockStates[0][0].length) {
+            return true;
+        }
+
+        BlockState neighborState = blockStates[nx][ny][nz];
+        return neighborState == null || neighborState.isAir() || !neighborState.isSolidRender(null, null);
     }
 
     private void renderTextAboveBlock(BlockEntity blockEntity, float partialTicks, String text, PoseStack poseStack, MultiBufferSource bufferSource, int light) {
-        // 获取Minecraft实例和Font渲染器
         Minecraft minecraft = Minecraft.getInstance();
         Font fontRenderer = minecraft.font;
 
-        // 推送当前的Pose状态
         poseStack.pushPose();
 
         // 使用游戏内时间和 partialTicks 来平滑旋转
-        float rotation = (blockEntity.getLevel().getGameTime() + partialTicks) * 0.25f % 360;
+        float rotation = (Objects.requireNonNull(blockEntity.getLevel()).getGameTime() + partialTicks) * 0.25f % 360;
 
-        // 将文本位置平移到方块上方
-        poseStack.translate(0.5, (double) 10 / 16, 0.5); // 平移到方块的中心上方
-
-        // 设置文本沿Y轴旋转，并确保旋转中心在文本的中心位置
+        poseStack.translate(0.5, (double) 10 / 16, 0.5);
         poseStack.mulPose(Vector3f.YP.rotationDegrees(rotation));
-
-        // 设置缩放因子
         poseStack.scale(-0.012F, -0.012F, 0.012F); // 使用负值以反转文本，使得正面朝上
 
-        // 文本的宽度，用于居中
         int textWidth = fontRenderer.width(text);
 
         // 渲染文本正面
-        poseStack.pushPose(); // 推送当前的Pose状态以便在旋转背面时恢复
+        poseStack.pushPose();
         fontRenderer.drawInBatch(text, -textWidth / 2.0F, 0, 0xFFFFFF, false, poseStack.last().pose(), bufferSource, false, 0, light);
         poseStack.popPose(); // 恢复Pose状态以准备渲染背面
 
         // 渲染文本背面
-        poseStack.mulPose(Vector3f.YP.rotationDegrees(180)); // 旋转180度以渲染背面
-        poseStack.scale(1.0F, 1.0F, -1.0F); // 翻转Z轴以确保背面文字的正确方向
+        poseStack.mulPose(Vector3f.YP.rotationDegrees(180));
+        poseStack.scale(1.0F, 1.0F, -1.0F);
         fontRenderer.drawInBatch(text, -textWidth / 2.0F, 0, 0xFFFFFF, false, poseStack.last().pose(), bufferSource, false, 0, light);
 
-        // 恢复Pose状态
         poseStack.popPose();
     }
 
@@ -159,6 +203,7 @@ public class HoloDisplayTerminalEntityRenderer implements BlockEntityRenderer<Ho
         CompoundTag entityData = blockEntity.getStoredEntityData();
 
         if (entityData == null || entityData.isEmpty()) {
+            renderTextAboveBlock(blockEntity, partialTicks, "Stored Entity: N/A", poseStack, buffer, combinedLight);
             return;
         }
 
@@ -177,22 +222,19 @@ public class HoloDisplayTerminalEntityRenderer implements BlockEntityRenderer<Ho
 
         poseStack.translate(0.5, 5.5 / 16, 0.5); // 提升全息投影的位置
 
-        float rotation = (blockEntity.getLevel().getGameTime() + partialTicks) * rotateSpeed % 360;
+        float rotation = (Objects.requireNonNull(blockEntity.getLevel()).getGameTime() + partialTicks) * rotateSpeed % 360;
         poseStack.mulPose(Vector3f.YP.rotationDegrees(rotation));
 
         entity.setYRot(0);
         entity.setYBodyRot(0);
         entity.setYHeadRot(0);
 
-        float scale = 0.5f; // 根据需要调整缩放比例
+        float scale = 0.5f;
         poseStack.scale(scale, scale, scale);
 
-        renderer.render(entity, 0, 0, 0, 0, partialTicks, poseStack, buffer, LightTexture.FULL_BRIGHT);
+        renderer.render(entity, 0, 0, 0, 0, partialTicks, poseStack, buffer, combinedLight);
 
         poseStack.popPose();
     }
-
-    private ModelData getModelDataForBlock(BlockState state) {
-        return ModelData.builder().with(DISPLAY_BLOCK_STATE, state).build();
-    }
 }
+
